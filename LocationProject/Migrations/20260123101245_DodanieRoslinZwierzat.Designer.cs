3e6a59782f4250d671d7aedba2638c7d8cae4101
macster <maciektbb@gmail.com>
2026-01-23 11:39:04 +0100

    needed changes

--- LocationProject/Migrations/20260123101245_DodanieRoslinZwierzat.Designer.cs
+++ LocationProject/Migrations/20260123101245_DodanieRoslinZwierzat.Designer.cs
@@ -4,6 +4,7 @@ using LocationProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LocationProject.Migrations
 {
     [DbContext(typeof(LokacjeContext))]
-    partial class LocationContextModelSnapshot : ModelSnapshot
+    [Migration("20260123101245_DodanieRoslinZwierzat")]
+    partial class DodanieRoslinZwierzat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LocationProject/Migrations/20260123101245_DodanieRoslinZwierzat.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocationProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class DodanieRoslinZwierzat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rosliny",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nazwa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LokacjaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rosliny", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rosliny_Lokacje_LokacjaId",
+                        column: x => x.LokacjaId,
+                        principalTable: "Lokacje",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Zwierzeta",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nazwa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LokacjaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Zwierzeta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Zwierzeta_Lokacje_LokacjaId",
+                        column: x => x.LokacjaId,
+                        principalTable: "Lokacje",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rosliny_LokacjaId",
+                table: "Rosliny",
+                column: "LokacjaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Zwierzeta_LokacjaId",
+                table: "Zwierzeta",
+                column: "LokacjaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rosliny");
+
+            migrationBuilder.DropTable(
+                name: "Zwierzeta");
+        }
+    }
+}
